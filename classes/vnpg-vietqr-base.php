1b139c5ab9689805e6fe8b779421ef20308196ca
Thuan Bui <thuanbui87@gmail.com>
2022-11-03 06:56:42 +0700

    Restructure folder and add options page

---
+++ classes/vnpg-vietqr-base.php
@@ -0,0 +1,14 @@
+<?php
+/*
+*
+* WC Base Payment Gateway
+*
+*/
+
+if (!defined('ABSPATH')) exit;
+
+if (!class_exists('WC_Payment_Gateway')) return;
+
+abstract class WC_VNPG_VietQR_Base extends WC_Payment_Gateway {
+
+}
